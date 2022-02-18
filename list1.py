4fa5a933e850e32258ec792e6677f7b7cce0b527
Om <80634429+Om17-cmd@users.noreply.github.com>
2022-02-18 22:12:42 +0530

    added list1 (solved list in a recursive method)

---
+++ list1.py
@@ -0,0 +1,10 @@
+Choices = ["mouse",
+           "computer",
+           "moniotr",
+           "mousepad",
+           "HDMI"
+           ]
+emp = []
+for i in range (1, len(Choices)+1):
+    emp.append(str(i))
+print(emp)
