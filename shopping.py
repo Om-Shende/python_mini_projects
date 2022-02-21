91fba3cd68cdcb9fc3cabadf04a9408978d13321
Om <80634429+Om17-cmd@users.noreply.github.com>
2022-02-21 23:47:15 +0530

    added shopping using iterative

---
+++ shopping.py
@@ -0,0 +1,11 @@
+shopiing_list = ["milk", "chair", "beer", "pendrive"]
+
+for items in shopiing_list:
+    if items == "pendrive" :
+        continue
+
+    print(items)
+
+#for items in shopiing_list:
+ #   if items != "pendrive" :
+  #      print(items)
